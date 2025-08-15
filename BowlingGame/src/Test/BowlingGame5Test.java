fc386ab37a6f76acef88a064cead363beee0c010
nathandickinson32 <nathandickinson32@gmail.com>
2025-08-15 17:43:42 -0400

    newest run through

--- BowlingGame/src/Test/BowlingGame5Test.java
+++ BowlingGame/src/Test/BowlingGame5Test.java
@@ -1,23 +1,18 @@
 package Test;
 
-import Core.BowlingGame4;
+import Core.BowlingGame5;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class BowlingGame4Test {
+public class BowlingGame5Test {
 
-    BowlingGame4 game = new BowlingGame4();
+    BowlingGame5 game = new BowlingGame5();
 
     private void rollMany(int rolls, int pins) {
         for (int i = 0; i < rolls; i++) {
             game.roll(pins);
         }
-    }
-
-    @Test
-    public void testNewGame() {
-        assertEquals(0, game.score());
     }
 
     @Test
@@ -33,39 +28,36 @@ public class BowlingGame4Test {
     }
 
     @Test
-    public void testOneFrame() {
-        game.roll(5);
-        game.roll(4);
-        assertEquals(9, game.score());
+    public void testAllOnes() {
+        rollMany(20, 1);
+        assertEquals(20, game.score());
     }
 
-   @Test
+    @Test
     public void testSpare() {
         game.roll(5);
         game.roll(5);
         game.roll(4);
-        rollMany(17, 0);
         assertEquals(18, game.score());
-   }
+    }
 
-   @Test
+    @Test
     public void testStrike() {
         game.roll(10);
         game.roll(5);
         game.roll(4);
         assertEquals(28, game.score());
-   }
+    }
 
-   @Test
-    public void testPerfectGame(){
+    @Test
+    public void testPerfectGame() {
         rollMany(12, 10);
         assertEquals(300, game.score());
-   }
+    }
 
-   @Test
-    public void testAlmostPerfectGame(){
+    @Test
+    public void testAlmostPerfectGame() {
         rollMany(9, 10);
         assertEquals(240, game.score());
-   }
-
+    }
 }
