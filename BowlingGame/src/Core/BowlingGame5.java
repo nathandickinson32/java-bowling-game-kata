c43a043fc0eaf7a4e52dcd3dd6ae24bb3e76ffc0
nathandickinson32 <nathandickinson32@gmail.com>
2025-08-18 09:05:54 -0400

    new run through

--- BowlingGame/src/Core/BowlingGame5.java
+++ BowlingGame/src/Core/BowlingGame5.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 
 public class BowlingGame5 {
 
+//    private ArrayList<Integer> rolls = new ArrayList<>();
+//
+//    public void roll(int pins) {
+//        rolls.add(pins);
+//    }
+
     private ArrayList<Integer> rolls = new ArrayList<>();
+
 
     public void roll(int pins) {
         rolls.add(pins);
@@ -17,6 +24,7 @@ public class BowlingGame5 {
 
         for (; i < rolls.size() - 2 && frame < 9; i += 2) {
             score += scoreFrame(i);
+
             if (isStrikeOrSpare(i))
                 score += rolls.get(i + 2);
 
@@ -24,7 +32,7 @@ public class BowlingGame5 {
             frame++;
         }
 
-        score += scoreFinalFrame(i, frame);
+        score += scoreLastFrames(i, frame);
         return score;
     }
 
@@ -38,7 +46,7 @@ public class BowlingGame5 {
         return rolls.get(i) == 10;
     }
 
-    private int scoreFinalFrame(int i, int frame) {
+    private int scoreLastFrames(int i, int frame) {
         int score = 0;
         for (; i < rolls.size(); i++) {
             if (isStrike(i) && frame < 9 && i < rolls.size() - 1)
@@ -55,4 +63,53 @@ public class BowlingGame5 {
     private int scoreFrame(int i) {
         return rolls.get(i) + rolls.get(i + 1);
     }
+
+
+//
+//    public int score() {
+//        int score = 0;
+//        int i = 0;
+//        int frame = 0;
+//
+//        for (; i < rolls.size() - 2 && frame < 9; i += 2) {
+//            score += scoreFrame(i);
+//
+//            if (isStrikeOrSpare(i))
+//                score += rolls.get(i + 2);
+//
+//            i += strikeFrameAdjustment(i);
+//            frame++;
+//        }
+//
+//        score += scoreRemainingRolls(i, frame);
+//        return score;
+//    }
+//
+//    private int strikeFrameAdjustment(int i) {
+//        if (isStrike(i))
+//            return -1;
+//        return 0;
+//    }
+//
+//    private boolean isStrike(int i) {
+//        return rolls.get(i) == 10;
+//    }
+//
+//    private int scoreRemainingRolls(int i, int frame) {
+//        int score = 0;
+//        for (; i < rolls.size(); i++) {
+//            if (isStrike(i) && frame < 9 && i < rolls.size() - 1)
+//                score += rolls.get(i + 1);
+//            score += rolls.get(i);
+//        }
+//        return score;
+//    }
+//
+//    private boolean isStrikeOrSpare(int i) {
+//        return scoreFrame(i) >= 10;
+//    }
+//
+//    private int scoreFrame(int i) {
+//        return rolls.get(i) + rolls.get(i + 1);
+//    }
 }
--- BowlingGame/src/Test/BowlingGame5Test.java
+++ BowlingGame/src/Test/BowlingGame5Test.java
@@ -7,13 +7,30 @@ import static org.junit.Assert.assertEquals;
 
 public class BowlingGame5Test {
 
-    BowlingGame5 game = new BowlingGame5();
+//    private BowlingGame5 game = new BowlingGame5();
+//
+//    private void rollMany(int rolls, int pins) {
+//        for (int i = 0; i < rolls; i++) {
+//            game.roll(pins);
+//        }
+//    }
+
+
+//
+//    @Test
+//    public void testGutterGame() {
+//        rollMany(20, 0);
+//        assertEquals(0, game.score());
+//    }
+//
 
     private void rollMany(int rolls, int pins) {
         for (int i = 0; i < rolls; i++) {
             game.roll(pins);
         }
     }
+
+    private BowlingGame5 game = new BowlingGame5();
 
     @Test
     public void testGutterGame() {
@@ -29,7 +46,7 @@ public class BowlingGame5Test {
 
     @Test
     public void testAllOnes() {
-        rollMany(20, 1);
+        rollMany(20,1);
         assertEquals(20, game.score());
     }
 
@@ -50,14 +67,55 @@ public class BowlingGame5Test {
     }
 
     @Test
-    public void testPerfectGame() {
+    public void testPerfectGame(){
         rollMany(12, 10);
         assertEquals(300, game.score());
     }
 
     @Test
-    public void testAlmostPerfectGame() {
+    public void testAlmostPerfectGame(){
         rollMany(9, 10);
         assertEquals(240, game.score());
     }
+
+
+//    @Test
+//    public void testOnePin() {
+//        game.roll(1);
+//        assertEquals(1, game.score());
+//    }
+//
+//    @Test
+//    public void testAllOnes() {
+//        rollMany(20, 1);
+//        assertEquals(20, game.score());
+//    }
+//
+//    @Test
+//    public void testSpare() {
+//        game.roll(5);
+//        game.roll(5);
+//        game.roll(4);
+//        assertEquals(18, game.score());
+//    }
+//
+//    @Test
+//    public void testStrike() {
+//        game.roll(10);
+//        game.roll(5);
+//        game.roll(4);
+//        assertEquals(28, game.score());
+//    }
+//
+//    @Test
+//    public void testPerfectGame() {
+//        rollMany(12, 10);
+//        assertEquals(300, game.score());
+//    }
+//
+//    @Test
+//    public void testAlmostPerfectGame() {
+//        rollMany(10, 10);
+//        assertEquals(270, game.score());
+//    }
 }
